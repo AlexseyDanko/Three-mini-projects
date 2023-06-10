11e0dcf8bb075786132533a0f6b10e53c65f6895
Alexsey <114829983+AlexseyDanko@users.noreply.github.com>
2023-06-10 09:55:42 +0300

    Update and rename VideoAudioYouTube.py to VideoFromYouTube.py

---
+++ VideoFromYouTube.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+from art import tprint
+
+# link = input('insert url:')
+def download_video(link=""):
+    tprint('>>Download>>Video>>function>>start>>', 'small')
+
+
+def main():
+    link = input('Please insert url: ')
+    download_video(link=link)
+    url = YouTube(link)
+    video = url.streams.get_highest_resolution()
+    tprint('Download process......', 'small')
+    video.download()
+    tprint('>>>File>>>Uploaded>>>')
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
