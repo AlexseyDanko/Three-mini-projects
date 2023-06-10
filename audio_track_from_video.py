2efb8cd4e24bc9aeed4190b61c6bfc4f2f409321
Alexsey <114829983+AlexseyDanko@users.noreply.github.com>
2023-06-10 10:26:14 +0300

    Update audio_track_from_video.py

--- audio_track_from_video.py
+++ audio_track_from_video.py
@@ -1,8 +1,17 @@
 import moviepy.editor
-from pathlib import Path
+from art import tprint
 
-video_file = Path('Скамейка.mp4')
+def cut_out_audio(video_file=""):
+    tprint('Download Cut_out_Audio function start', 'small')
 
-video = moviepy.editor.VideoFileClip(f'{video_file}')
-audio = video.audio
-audio.write_audiofile(f'{video_file.stem}.mp3')
+
+def main():
+    video_file = (input('Please insert path file avi: '))
+    cut_out_audio(video_file=video_file)
+    video = moviepy.editor.VideoFileClip(f'{video_file}')
+    audio = video.audio
+    audio.write_audiofile(f'{video_file}.mp3')
+
+
+if __name__ == '__main__':
+    main()
