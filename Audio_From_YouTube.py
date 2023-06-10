d0f39feb32ec10df030bf852072edde5de1d2a6a
Alexsey <114829983+AlexseyDanko@users.noreply.github.com>
2023-06-10 10:00:54 +0300

    Create Audio_From_YouTube.py

---
+++ Audio_From_YouTube.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+from art import tprint
+
+# link = input('insert url:')
+def download_audio(link=""):
+    tprint('>>Download>>Video>>function>>start>>', 'small')
+
+
+def main():
+    link = input('Please insert url: ')
+    download_audio(link=link)
+    url = YouTube(link)
+    audio = url.streams.get_audio_only()
+    tprint('Download process......', 'small')
+    audio.download()
+    tprint('>>>File>>>Uploaded>>>')
+
+
+if __name__ == '__main__':
+    main()
