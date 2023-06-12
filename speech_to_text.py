acff5e80743d8831e3a7e6af760dfa66f3eb672c
Alexsey <114829983+AlexseyDanko@users.noreply.github.com>
2023-06-12 23:42:09 +0300

    Create speech_to_text.py

---
+++ speech_to_text.py
@@ -0,0 +1,21 @@
+import speech_recognition
+import time
+
+
+def main():
+    rec = speech_recognition.Recognizer()
+    microfone = speech_recognition.Microphone()
+    unx_time = int(time.time())
+
+    with microfone as source:
+        rec.adjust_for_ambient_noise(source)
+        print('Please speak text: ')
+        audio = rec.listen(source)
+    text = rec.recognize_google(audio, language='ru-RU')
+    with open(f'{unx_time}.txt', 'w', encoding='utf-8') as file:
+        file.write(text)
+    print('File has been created: ')
+
+
+if __name__ == '__main__':
+    main()
