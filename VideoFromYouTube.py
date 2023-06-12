eb4d77eca85cb87e304ceec37ee666140f8486e0
Alexsey <114829983+AlexseyDanko@users.noreply.github.com>
2023-06-12 23:15:25 +0300

    Update VideoFromYouTube.py

--- VideoFromYouTube.py
+++ VideoFromYouTube.py
@@ -1,14 +1,10 @@
 from pytube import YouTube
 from art import tprint
 
-# link = input('insert url:')
+
+
 def download_video(link=""):
     tprint('>>Download>>Video>>function>>start>>', 'small')
-
-
-def main():
-    link = input('Please insert url: ')
-    download_video(link=link)
     url = YouTube(link)
     video = url.streams.get_highest_resolution()
     tprint('Download process......', 'small')
@@ -16,7 +12,9 @@ def main():
     tprint('>>>File>>>Uploaded>>>')
 
 
+def main():
+    download_video(link=input('Please insert url: '))
+
+
 if __name__ == '__main__':
     main()
-
-
