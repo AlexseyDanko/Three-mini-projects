cd584182c71abe6dc312fa99963b36a64e649ee2
Alexsey <114829983+AlexseyDanko@users.noreply.github.com>
2023-06-12 23:12:45 +0300

    Update Audio_From_YouTube.py

--- Audio_From_YouTube.py
+++ Audio_From_YouTube.py
@@ -1,19 +1,18 @@
 from pytube import YouTube
 from art import tprint
 
-# link = input('insert url:')
+
 def download_audio(link=""):
     tprint('>>Download>>Audio>>function>>start>>', 'small')
-
-
-def main():
-    link = input('Please insert url: ')
-    download_audio(link=link)
     url = YouTube(link)
     audio = url.streams.get_audio_only()
     tprint('Download process......', 'small')
     audio.download()
     tprint('>>>File>>>Uploaded>>>')
+
+
+def main():
+    download_audio(link=input('Please insert url: '))
 
 
 if __name__ == '__main__':
