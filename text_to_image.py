2cc7ca038a805058321b4f669485ab12cd901951
Alexsey <114829983+AlexseyDanko@users.noreply.github.com>
2023-06-25 15:00:36 +0300

    Create text_to_image.py

---
+++ text_to_image.py
@@ -0,0 +1,22 @@
+import openai
+from api import api_key # better to use OS
+import json
+from base64 import b64decode
+
+prompt = input('The prompt:')
+openai.api_key = api_key
+
+response = openai.Image.create(
+    prompt=prompt,
+    n=1,
+    size='1024x1024',
+    response_format='b64_json'
+)
+with open('base.json', 'w') as file:
+    json.dump(response, file, indent=4, ensure_ascii=False)
+
+image_base = b64decode(response['data'][0]['b64_json'])
+file_name = '_'.join(prompt.split(' '))
+
+with open(f'{file_name}.png', 'wb') as file:
+    file.write(image_base)
