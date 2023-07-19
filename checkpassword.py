29807e85d864a9d1e24c9b289dc520c5aa03c073
Alexsey <114829983+AlexseyDanko@users.noreply.github.com>
2023-07-19 23:12:28 +0300

    Update checkpassword.py

--- checkpassword.py
+++ checkpassword.py
@@ -1,25 +1,25 @@
 def check_password_strength(password):
     score = 0
 
-    # Проверка длины пароля
+    # проверка длины пароля
     if len(password) < 8:
         return 0
 
     score += len(password) // 2
 
-    # Проверка наличия цифр в пароле
+    # проверка наличия цифр в пароле
     if any(char.isdigit() for char in password):
         score += 2
 
-    # Проверка наличия букв в верхнем регистре и нижнем регистре в пароле
+    # проверка наличия букв в верхнем регистре и нижнем регистре в пароле
     if any(char.isupper() for char in password) and any(char.islower() for char in password):
         score += 3
 
-    # Проверка наличия специальных символов в пароле
+    # проверка наличия специальных символов в пароле
     if any(not char.isalnum() for char in password):
         score += 4
 
-    # Ограничение максимального балла до 10
+    # ограничение максимального балла до 10
     if score > 10:
         score = 10
 
